714b7af48e56352152dbe79081a38c1e96ef9124
yadarochka <danil2383904@yandex.ru>
2024-10-19 23:33:46 +0500

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новости банка");?>
+
+
+<p>Content</p>
+
+
+
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
